8c61f62db7e653fe8938626ba07ecd5a0ab2207a
Raphassit Leelachaikul <raphassitlee@gmail.com>
2017-12-22 12:06:53 +0700

    RP web shows request ID

--- rp/web_files/script.js
+++ rp/web_files/script.js
@@ -6,40 +6,41 @@ const socket = io('http://localhost:8080');
 
 let requestId = null;
 
-const status = document.getElementById('status');
+const requestIdElement = document.getElementById('requestId');
+const statusElement = document.getElementById('status');
 
 const verifyButton = document.getElementById('verify');
 verifyButton.addEventListener('click', (event) => {
-  status.textContent = 'Pending...';
-  status.style = '';
+  statusElement.textContent = 'Pending...';
+  statusElement.style = '';
   fetch('/verifyIdentity').then((response) => {
     return response.json();
   }).then((json) => {
     requestId = json.requestId;
+    requestIdElement.textContent = 'Request ID: ' + requestId;
   });
 });
 
 const verifyHideSourceRpButton = document.getElementById('verifyHideSourceRp');
 verifyHideSourceRpButton.addEventListener('click', (event) => {
-  status.textContent = 'Pending...';
-  status.style = '';
+  statusElement.textContent = 'Pending...';
+  statusElement.style = '';
   fetch('/verifyIdentity?hideSourceRp=true').then((response) => {
     return response.json();
   }).then((json) => {
     requestId = json.requestId;
+    requestIdElement.textContent = 'Request ID:' + requestId;
   });
 });
 
 socket.on('success', function(data){
   if (data.requestId === requestId) {
-    status.textContent = 'Success!';
-    requestId = null;
+    statusElement.textContent = 'Success!';
   }
 });
 
 socket.on('fail', function(data){
   if (data.requestId === requestId) {
-    status.textContent = 'Failed!';
-    requestId = null;
+    statusElement.textContent = 'Failed!';
   }
 });
