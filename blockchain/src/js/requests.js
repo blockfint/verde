b21efac48a37acd07d98cd1981d669e911efe97d
Nick Kanakakorn <nick@blockfint.com>
2017-12-28 15:49:49 +0700

    First draft

---
+++ blockchain/src/js/requests.js
@@ -0,0 +1,32 @@
+/*
+ * Create a request.
+ * Parameters
+ *   userName : string
+ *   requestText : string
+ * Returns
+ *   requestID : string
+*/
+
+function createRequest(requestObject) {
+}
+
+/* 
+ * Parameters
+ *   Function
+ *     
+ * Example
+ *   watchRequestEvent(function(error, result)) {
+ *     if (!error)
+ *       console.log(result)
+ *     else
+ *       console.error(error);
+ */
+
+function watchRequestEvent() {
+}
+
+function watchIDPResponseEvent() {
+}
+
+function watchAuthenticationEvent() {
+}
