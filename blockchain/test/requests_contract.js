e7d72d0ae995c8ed188085512aa15154ea947a3b
Nick Kanakakorn <nick@blockfint.com>
2018-01-09 18:37:52 +0700

    Improved the test codes.

--- blockchain/test/requests_contract.js
+++ blockchain/test/requests_contract.js
@@ -1,23 +1,27 @@
-var Requests = artifacts.require("./Requests.sol");
+var Requests = artifacts.require('./Requests.sol');
 
 contract('Requests', function(accounts) {
   let requests
-  it("should create two requests", function() {
-    return Requests.deployed().then(function(instance) {
+
+  before('set up requests', (done) => {
+    Requests.deployed().then((instance) => {
       requests = instance;
-      return requests.createRequest("0x1234","Release credit record", 1)
-    }).then(function(rval) {
-      console.log("rval " + rval);
-      return requests.getRequestCount();
-    }).then(function(requestCount) {
-      console.log("count " + requestCount);
-      assert.equal(requestCount, 1, "Count");
-    }).then(function() {
-      requests.createRequest("0x1235","Own car?", 1)
-      return requests.getRequestCount();
-    }).then(function(requestCount) {
-      console.log("count " + requestCount);
-      assert.equal(requestCount, 2, "Count");
-    });
+    }).then(() => done())
+  })
+
+  it('should create two requests', async() => {
+      let requestCount1 = await requests.getRequestCount();
+      let request1 = await requests.createRequest(
+        '0x1234','Release credit record', 1)
+      let requestCount2 = await requests.getRequestCount();
+      console.log('rcount1 ' + requestCount1 + ', type: ' + typeof(requestCount1));
+      console.log('rcount2 ' + requestCount2 + ', type: ' + typeof(requestCount2));
+      console.log('requestCount diff:' + parseInt(requestCount2 - requestCount1));
+      assert.equal(1, requestCount2 - requestCount1, 'Count');
+ 
+      let request2 = await requests.createRequest('0x1235','Own car?', 1);
+      requestCount2 = await requests.getRequestCount();
+      assert.equal(2, requestCount2 - requestCount1, 'Count');
+      assert.notEqual(request2, request1, 'request ID should not be the same')
   });
 });
