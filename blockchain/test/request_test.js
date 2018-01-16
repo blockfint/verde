ab2796da39b892ef7e6d74db4fca9c7460b1a627
Nick Kanakakorn <nick@blockfint.com>
2018-01-16 14:49:39 +0700

    Add getter tests.

--- blockchain/test/request_test.js
+++ blockchain/test/request_test.js
@@ -7,32 +7,35 @@ contract('Request', function(accounts) {
   let request;
 
   before('set up request', (done) => {
-    Request.new(accounts[0], accounts[2], 'rp condition', 'Request123').then((instance) => {
+    Request.new().then((instance) => {
       request = instance;
-      console.log('request instance:' + JSON.stringify(request));
+      console.log('request instance:' + request);
       done();
     });
   });
 
+  let rpAddress = accounts[0];
+  let userAddress = accounts[2];
+  let requestText = 'Request 123';
+  let rpCondition = 'RP Condition';
   it('should have all getters with correct value', async () => {
+    await request.newRequest(rpAddress, userAddress, rpCondition, requestText, 
+                             [], []);
     console.log('request instance 2:' + request);
     console.log('rp address', await request.rpAddress());
     console.log('user address', await request.userAddress());
-    // assert.equal(await request.requestText(), 'Request123', 'requestText()')
-    // use did 
-    // Here we get an error because request is a string but the smart contract
-    // treat is as object
-    // await did.addIdpResponse(request, 0, 'OK');
-    // console.log("Added IDP Response");
-    let result = await request.authenticationComplete();
-    assert.equal(false, result, 'Should not complete yet.')
+    assert.equal(rpAddress, await request.rpAddress(), 'RP address');
+    assert.equal(userAddress, await request.userAddress(), 'User address');
+    assert.equal(requestText, await request.requestText());
+    assert.equal(rpCondition, await request.rpCondition());
+    assert.equal(false, await request.authenticationComplete(), 
+                 'Should not complete yet.');
 
     // use request object directly. The responder would be accounts[1].
   });
 
   let res;
   it('should have idp response', async () => {
-    await request.newRequest(accounts[0], accounts[2], 'rp condition', 'Request123', [], []);
     res = await request.addIdpResponse(accounts[1], 0, 'OK: IDP1');
     console.log('res1: ' + JSON.stringify(res));
     res = await request.getIdpResponse();
