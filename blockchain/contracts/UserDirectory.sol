f356ae3fd966758396b3e0ffde01a69004f5b43e
Sakkarin <Sakkarin@admins-MacBook-Air.local>
2018-01-19 18:32:53 +0700

    Fix follow eslint

--- blockchain/contracts/UserDirectory.sol
+++ blockchain/contracts/UserDirectory.sol
@@ -1,23 +1,23 @@
 pragma solidity ^0.4.17;
 
 contract UserDirectory {
-	mapping (string => address) private _users;
-	uint public userCount;
+  mapping (string => address) private _users;
+  uint public userCount;
 
-	function UserDirectory() public {
-		userCount = 0;
-	}
+  function UserDirectory() public {
+    userCount = 0;
+  }
 
-	function newUser(
-		string _id,
-		address _userContractAddress
-	    ) public {
-			_users[_id] = _userContractAddress;
-			userCount++;
-	}
+  function newUser(
+    string _id,
+    address _userContractAddress
+    ) public {
+      _users[_id] = _userContractAddress;
+      userCount++;
+    }
 
-	function findUserByID(string id) 
-			public view returns (address userContractAddress) {
-		return (userContractAddress = _users[id]);
-	}
+    function findUserByID(string id) 
+      public view returns (address userContractAddress) {
+    return (userContractAddress = _users[id]);
+  }
 }
--- blockchain/test/userDirectory_test.js
+++ blockchain/test/userDirectory_test.js
@@ -1,10 +1,11 @@
+/* global web3:true, assert:true, artifacts:true, contract:true */
+/* eslint-env mocha */
+
 var UserDirectory = artifacts.require('UserDirectory');
 var User = artifacts.require('User');
-var accounts = web3.eth.accounts;
 
 contract('UserDirectory', function(accounts) {
   let userDirectory;
-  let condition;
   let user1;
   let user2;
 
@@ -29,12 +30,12 @@ contract('UserDirectory', function(accounts) {
     });
   });
 
-  let user1_id = "1111";
-  let user1_namespace = "aaaa";
+  let user1_id = '1111';
+  let user1_namespace = 'aaaa';
   let user1_owner = accounts[0];
 
-  let user2_id = "2222";
-  let user2_namespace = "bbbb";
+  let user2_id = '2222';
+  let user2_namespace = 'bbbb';
   let user2_owner = accounts[1];
 
   it('should have all getters with correct value', async () => {
@@ -48,28 +49,28 @@ contract('UserDirectory', function(accounts) {
 
     // get Contract address from UserDirectory
     var user1_ContractAddr = await userDirectory.findUserByID(user1_id);
-    assert.equal(user1_ContractAddr, user1.address, "User 1 contract address will be the same.");
+    assert.equal(user1_ContractAddr, user1.address, 'User 1 contract address will be the same.');
 
     var user2_ContractAddr = await userDirectory.findUserByID(user2_id);
-    assert.equal(user2_ContractAddr, user2.address, "User 2 contract address will be the same.");
+    assert.equal(user2_ContractAddr, user2.address, 'User 2 contract address will be the same.');
 
     // test wrong User ID
-    var wrong_ContractAddr = await userDirectory.findUserByID("3333");
-    assert.equal(web3.toDecimal(wrong_ContractAddr), 0, "Should be get 0.");
+    var wrong_ContractAddr = await userDirectory.findUserByID('3333');
+    assert.equal(web3.toDecimal(wrong_ContractAddr), 0, 'Should be get 0.');
 
     // Should have correct value
     var new_user1 = User.at(user1_ContractAddr);
-    assert.equal(user1_id, await new_user1.id(), "id");
-    assert.equal(user1_namespace, await new_user1.namespace(), "namespace");
-    assert.equal(user1_owner, await new_user1.owner(), "owner");
+    assert.equal(user1_id, await new_user1.id(), 'id');
+    assert.equal(user1_namespace, await new_user1.namespace(), 'namespace');
+    assert.equal(user1_owner, await new_user1.owner(), 'owner');
 
     var new_user2 = User.at(user2_ContractAddr);
-    assert.equal(user2_id, await new_user2.id(), "id");
-    assert.equal(user2_namespace, await new_user2.namespace(), "namespace");
-    assert.equal(user2_owner, await new_user2.owner(), "owner");
+    assert.equal(user2_id, await new_user2.id(), 'id');
+    assert.equal(user2_namespace, await new_user2.namespace(), 'namespace');
+    assert.equal(user2_owner, await new_user2.owner(), 'owner');
 
     // check get user count 
-    assert.equal(2, new web3.BigNumber(await userDirectory.userCount()).toString(), "User count");
+    assert.equal(2, new web3.BigNumber(await userDirectory.userCount()).toString(), 'User count');
 
   });
 
