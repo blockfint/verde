b825b1a8b0674de6984976b8f571fde6198f0dba
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-01-24 17:04:50 +0700

    UI for IDP website

--- idp/web_files/script.js
+++ idp/web_files/script.js
@@ -1,63 +1,128 @@
-function updateRequestList(json) {
-  const list = document.getElementById('list');
-  const approved = document.getElementById('approved');
-  const denied = document.getElementById('denied');
-  json.forEach(function(requestObject) {
-    if(!requestObject.processed) addRequest(requestObject,list)
-    else {
-      let tmpDiv = document.createElement("div");
-      var displayData = Object.assign({}, requestObject);
-      delete displayData.userId;
-      tmpDiv.innerHTML = JSON.stringify(displayData);
-      if(requestObject.approved) approved.appendChild(tmpDiv);
-      else denied.appendChild(tmpDiv);
-    }
-  });
-}
+const pendingList = document.getElementById('pendingList');
+const approvedList = document.getElementById('approved');
+const deniedList = document.getElementById('denied');
+const loadingIndicators = document.getElementsByClassName('loading-indicator');
 
 window.addEventListener('load', () => {
   var userId = window.location.href.split('/');
-  userId = userId[userId.length-1];
-  if(!userId) window.location = '/home/0x3355';
-  fetch('/getPendingList/' + userId).then((response) => {
-    return response.json();
-  }).then((json) => {
-    console.log(json);
-    updateRequestList(json);
-  }).catch(function(error) {
-    console.error(error);
-  });
+  userId = userId[userId.length - 1];
+  if (!userId) window.location = '/home/0x3355';
+  fetch('/getPendingList/' + userId)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      console.log(json);
+      hideLoadingIndicators();
+      updateRequestList(json);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
 });
 
-function addRequestButton(userId,requestId,action) {
-  var buttonElement = document.createElement("button");
-  buttonElement.innerHTML = action;
-  buttonElement.addEventListener('click', (event) => {
-    fetch('/' + action + '/',{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'requestId=' + requestId + '&userId=' + userId
-    }).then((response) => {
-      //return response;
-      //return response.json();
-      window.location.reload();
-    }).then((json) => {
-      console.log(json);
-    });
+function hideLoadingIndicators() {
+  Array.prototype.forEach.call(loadingIndicators, (ele) => {
+    ele.style = 'display:none;';
   });
-  return buttonElement;
 }
 
-function addRequest(requestObject,divElement) {
-  var tmpDiv = document.createElement("div");
-  var displayData = Object.assign({}, requestObject);
-  delete displayData.userId;
-  tmpDiv.innerHTML = JSON.stringify(displayData);
-  tmpDiv.appendChild(addRequestButton(requestObject.userId,requestObject.requestId,'approve'));
-  tmpDiv.appendChild(addRequestButton(requestObject.userId,requestObject.requestId,'deny'));
+function updateRequestList(json) {
+  if (Array.isArray(json)) {
+    if (json.length > 0) {
+      json.forEach(function(requestObject) {
+      const listItem = createListItem(requestObject);
 
-  divElement.appendChild(tmpDiv);
+      if (!requestObject.processed) {
+        pendingList.appendChild(listItem);
+      } else {
+        if (requestObject.approved) {
+          approvedList.appendChild(listItem);
+        } else {
+          deniedList.appendChild(listItem);
+        }
+      }
+    });
+    }
+  }
 
+  if (pendingList.children.length === 0) {
+    pendingList.appendChild(createEmptyTextListItem());
+  }
+  if (approvedList.children.length === 0) {
+    approvedList.appendChild(createEmptyTextListItem());
+  }
+  if (deniedList.children.length === 0) {
+    deniedList.appendChild(createEmptyTextListItem());
+  }
+}
+
+function createListItem(requestObject) {
+  let li = document.createElement('li');
+  li.classList.add('list-group-item');
+  li.classList.add('request-list-item');
+  // let displayData = Object.assign({}, requestObject);
+  // delete displayData.userId;
+
+  let infoDiv = document.createElement('div');
+  infoDiv.classList.add('request-info');
+  infoDiv.innerHTML = `<div>Request ID: ${requestObject.requestId}</div>
+    <div>RP ID: ${requestObject.rpId}</div>
+    <div>Data: ${requestObject.data}</div>`;
+  li.appendChild(infoDiv);
+
+  let buttonsDiv = document.createElement('div');
+  buttonsDiv.classList.add('request-buttons');
+  li.appendChild(buttonsDiv);
+
+  if (!requestObject.processed) {
+    buttonsDiv.appendChild(
+      createRequestButton(requestObject.userId, requestObject.requestId, 'approve')
+    );
+    buttonsDiv.appendChild(
+      createRequestButton(requestObject.userId, requestObject.requestId, 'deny')
+    );
+  }
+  return li;
+}
+
+function createEmptyTextListItem() {
+  let li = document.createElement('li');
+  li.classList.add('list-group-item');
+
+  li.textContent = 'No items';
+
+  return li;
+}
+
+function createRequestButton(userId, requestId, action) {
+  var buttonElement = document.createElement('button');
+  buttonElement.type = 'button';
+  buttonElement.classList.add('btn');
+  buttonElement.classList.add('btn-block');
+  if (action === 'approve') {
+    buttonElement.classList.add('btn-success');
+    buttonElement.textContent = 'Approve';
+  } else if (action === 'deny') {
+    buttonElement.classList.add('btn-danger');
+    buttonElement.textContent = 'Deny';
+  }
+  buttonElement.addEventListener('click', event => {
+    fetch('/' + action + '/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'requestId=' + requestId + '&userId=' + userId,
+    })
+      .then(response => {
+        //return response;
+        //return response.json();
+        window.location.reload();
+      })
+      .then(json => {
+        console.log(json);
+      });
+  });
+  return buttonElement;
 }
